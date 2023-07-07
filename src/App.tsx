0082e80a55d09f226fa28a05f454998690e07982
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-07 18:24:44 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,19 @@ import Content from './Components/MainComponents/Content';
 import Footer from './Components/MainComponents/Footer';
 import Header from './Components/MainComponents/Header';
 import './App.scss';
-import { Col, Grid, Row } from './GridSystem - typescript';
 
 function App(): JSX.Element {
    return (
-      <div>
-         <Grid>
-            <Row>
-               <Col size={['l-12']}>
-                  <div className="header">
-                     <Header />
-                  </div>
-               </Col>
-            </Row>
-            <Row>
-               <Col size={['l-12']}>
-                  <div className="content">
-                     <Content />
-                  </div>
-               </Col>
-            </Row>
-            <Row>
-               <Col size={['l-12']}>
-                  <div className="footer" id='footer'>
-                     <Footer />
-                  </div>
-               </Col>
-            </Row>
-         </Grid>
+      <div className="wrapper">
+         <div className="header">
+            <Header />
+         </div>
+         <div className="content">
+            <Content />
+         </div>
+         <div className="footer" id="footer">
+            <Footer />
+         </div>
       </div>
    );
 }
