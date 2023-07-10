5f5814c7b68eed1533886542777ee057330453ba
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-11 01:39:19 +0700

    .

--- src/Components/MainComponents/Header/Components/Navbar/Navbar.tsx
+++ src/Components/MainComponents/Header/Components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import classNames from 'classnames/bind';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-scroll';
 
 import styles from './Navbar.module.scss';
@@ -10,6 +10,7 @@ const cx = classNames.bind(styles);
 
 const Navbar: React.FC = () => {
    const [isActive, setIsActive] = useState(false);
+   const mobileNavbarRef = useRef<HTMLDivElement>(null);
 
    const handleMenuToggle = () => {
       setIsActive(!isActive);
@@ -21,6 +22,23 @@ const Navbar: React.FC = () => {
          checkbox.checked = false;
       }
    };
+
+   const handleClickOutside = (event: MouseEvent) => {
+      if (mobileNavbarRef.current && !mobileNavbarRef.current.contains(event.target as Node)) {
+         setIsActive(false);
+         const checkbox = document.getElementById('show-hide-navbar') as HTMLInputElement;
+         if (checkbox) {
+            checkbox.checked = false;
+         }
+      }
+   };
+
+   useEffect(() => {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+         document.removeEventListener('mousedown', handleClickOutside);
+      };
+   }, []);
 
    return (
       <>
@@ -63,7 +81,7 @@ const Navbar: React.FC = () => {
             <div className={cx('bar')}></div>
          </label>
          <input type="checkbox" name="" id="show-hide-navbar" className={cx('menu-toggle', 'hiden')} hidden />
-         <nav className={cx('mobile-navbar')}>
+         <nav className={cx('mobile-navbar')} ref={mobileNavbarRef}>
             <ul className={cx('mobile-navbar-list')}>
                <li className={cx('mobile-navbar-item')}>
                   <Link
