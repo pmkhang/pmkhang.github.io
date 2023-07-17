0e568769f93924c21aea3346e5f5238d07638982
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-17 15:49:44 +0700

    .

--- src/Components/MainComponents/Content/Components/AboutMe/AboutMe.tsx
+++ src/Components/MainComponents/Content/Components/AboutMe/AboutMe.tsx
@@ -4,7 +4,7 @@ import Typed from 'typed.js';
 
 import styles from './AboutMe.module.scss';
 import { Col, Grid, Row } from 'GridSystem - typescript';
-import brian from '../../../../../Assets/Images/brain.svg'
+import brian from '../../../../../Assets/Images/brain.svg';
 
 const cx = classNames.bind(styles);
 const AboutMe: React.FC = () => {
@@ -30,29 +30,29 @@ const AboutMe: React.FC = () => {
       <section id="aboutme" className={cx('wrapper')}>
          <Grid>
             <Row>
-                  <Col size={['l-6', 'm-12', 's-12']}>
-                     <div className={cx('info')}>
-                        <h2 className={cx('greeting')}>Hello!</h2>
-                        <div className={cx('info-text')}>
-                           <h2>I'm</h2>
-                           <h2>
-                              <strong ref={typedElementRef} className={cx('multi-text')}>
-                                 {' '}
-                              </strong>
-                           </h2>
-                        </div>
-                        <strong className={cx('info-description')}>
-                           As a proficient Front-End Developer with expertise in HTML, CSS , JavaScript and React, and a
-                           talented Graphic, UI-UX Designer with a deep understanding of visual design principles and have
-                           experience using tools such as Adobe Photoshop, Illustrator, InDesign and Figma.
-                        </strong>
+               <Col size={['l-6', 'm-12', 's-12']}>
+                  <div className={cx('info')}>
+                     <h2 className={cx('greeting')}>Hello!</h2>
+                     <div className={cx('info-text')}>
+                        <h2>I'm</h2>
+                        <h2>
+                           <strong ref={typedElementRef} className={cx('multi-text')}>
+                              {' '}
+                           </strong>
+                        </h2>
                      </div>
-                  </Col>
-                  <Col size={['l-6', 'm-12', 's-12']}>
-                     <div className={cx('img-desc')}>
-                        <img src={brian} alt="" />
-                     </div>
-                  </Col>
+                     <strong className={cx('info-description')}>
+                        As a proficient Front-End Developer with expertise in HTML, CSS , JavaScript and React, and a
+                        talented Graphic, UI-UX Designer with a deep understanding of visual design principles and have
+                        experience using tools such as Adobe Photoshop, Illustrator, InDesign and Figma.
+                     </strong>
+                  </div>
+               </Col>
+               <Col size={['l-6', 'm-12', 's-12']}>
+                  <div className={cx('img-desc')}>
+                     <img src={brian} alt="" />
+                  </div>
+               </Col>
             </Row>
          </Grid>
       </section>
