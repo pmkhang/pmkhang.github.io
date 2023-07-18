2eccc46d6f2b0b824f997c3fe87b6a79777cb76a
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-18 17:00:06 +0700

    edu #1

--- src/Components/MainComponents/Content/Components/Education/Education.tsx
+++ src/Components/MainComponents/Content/Components/Education/Education.tsx
@@ -2,17 +2,45 @@ import classNames from 'classnames/bind';
 
 import styles from './Education.module.scss';
 import { Col, Grid, Row } from 'GridSystem - typescript';
+import logoTDC from '~/Assets/Images/logoTDC.svg';
+import logoAptech from '~/Assets/Images/Logo-Aptech-Sua.webp';
 
 const cx = classNames.bind(styles);
-
 
 const Education: React.FC = () => {
    return (
       <section id="education" className={cx('wrapper')}>
          <Grid>
             <Row>
-               <Col size={['l-12', 'm-12', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-6', 'm-12', 's-12']}>
+                  <div className={cx('school-info')}>
+                     <div className={cx('school-logo')}>
+                        <img className={cx('logo')} src={logoTDC} alt="" />
+                     </div>
+                     <div className={cx('school-time')}>
+                        <span className={cx('time')}>2019 - 2022</span>
+                     </div>
+                     <div className={cx('school')}>
+                        <h2 className={cx('school-name')}>Thu Duc College of Technology</h2>
+                        <span className={cx('school-major')}>Graphic Design</span>
+                        <span className={cx('school-specialize')}>2D - Video Editor</span>
+                     </div>
+                  </div>
+               </Col>
+               <Col size={['l-6', 'm-12', 's-12']}>
+                  <div className={cx('school-info')}>
+                     <div className={cx('school-logo')}>
+                        <img className={cx('logo')} src={logoAptech} alt="" />
+                     </div>
+                     <div className={cx('school-time')}>
+                        <span className={cx('time')}>2023 - Present</span>
+                     </div>
+                     <div className={cx('school')}>
+                        <h2 className={cx('school-name')}>Aptech Computer Education</h2>
+                        <span className={cx('school-major')}>Infomation Technology</span>
+                        <span className={cx('school-specialize')}>Software Engineer</span>
+                     </div>
+                  </div>
                </Col>
             </Row>
          </Grid>
--- src/GridSystem - typescript/GridSystem.tsx
+++ src/GridSystem - typescript/GridSystem.tsx
@@ -6,42 +6,48 @@ const cx = classNames.bind(styles);
 
 interface GridProps {
    children: ReactNode;
+   className?: string; 
 }
 
-export const Grid: React.FC<GridProps> = ({ children }) => {
-   return <div className={cx('grid')}>{children}</div>;
+export const Grid: React.FC<GridProps> = ({ children, className }) => {
+   const gridClassName = cx('grid', className); 
+   return <div className={gridClassName}>{children}</div>;
 };
 
-export const GridWide: React.FC<GridProps> = ({ children }) => {
-   return <div className={cx('grid', 'wide')}>{children}</div>;
+export const GridWide: React.FC<GridProps> = ({ children, className }) => {
+   const gridWideClassName = cx('grid', 'wide', className); 
+   return <div className={gridWideClassName}>{children}</div>;
 };
 
-export const Row: React.FC<GridProps> = ({ children }) => {
-   return <div className={cx('row')}>{children}</div>;
+export const Row: React.FC<GridProps> = ({ children, className }) => {
+   const rowClassName = cx('row', className); 
+   return <div className={rowClassName}>{children}</div>;
 };
 
-export const RowNoGutter: React.FC<GridProps> = ({ children }) => {
-   return <div className={cx('row','no-gutters')}>{children}</div>;
+export const RowNoGutter: React.FC<GridProps> = ({ children, className }) => {
+   const rowNoGutterClassName = cx('row', 'no-gutters', className); 
+   return <div className={rowNoGutterClassName}>{children}</div>;
 };
 
-interface ColProps {
+interface ColProps extends GridProps {
    size: string[];
    offset?: string[];
-   children: ReactNode;
 }
 
-export const Col: React.FC<ColProps> = ({ size, offset, children }) => {
+export const Col: React.FC<ColProps> = ({ size, offset, children, className }) => {
    const colClasses = size.map((s) => cx('col', s));
    const offsetClasses = offset && offset.map((o) => cx(`offset-${o.replace('-o-', '-')}`));
-   const colClassName = cx(colClasses, offsetClasses);
+   const colClassName = cx(colClasses, offsetClasses, className); 
 
    return <div className={colClassName}>{children}</div>;
 };
 
-//<Col size={['l-6', 'm-6', 's-6']} offset={['l-o-2', 'm-o-4', 's-o-6']}>
-//   Content 1
-// </Col>
-
-// <Col size={['l-6', 'm-6', 's-6']} offset={['l-o-2', 'm-o-4', 's-o-6']}>
-//   Content 2
-// </Col>
+// Usage example
+// <Row className="custom-row-class">
+//   <Col size={['l-6', 'm-6', 's-6']} offset={['l-o-2', 'm-o-4', 's-o-6']} className="custom-col-class">
+//     Content 1
+//   </Col>
+//   <Col size={['l-6', 'm-6', 's-6']} offset={['l-o-2', 'm-o-4', 's-o-6']} className="custom-col-class">
+//     Content 2
+//   </Col>
+// </Row>
