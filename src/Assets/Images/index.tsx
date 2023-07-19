0b8185e2731b66548ab7fbdf145959455dc4f5de
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-19 17:18:28 +0700

    Skill #1

---
+++ src/Assets/Images/index.tsx
@@ -0,0 +1,13 @@
+const images = {
+   logo: require('~/Assets/Images/logo2.svg').default,
+   html: require('~/Assets/Images/html.svg').default,
+   css: require('~/Assets/Images/css.svg').default,
+   js: require('~/Assets/Images/js.svg').default,
+   react: require('~/Assets/Images/react.svg').default,
+   ps: require('~/Assets/Images/photoshop.svg').default,
+   ai: require('~/Assets/Images/illustrator.svg').default,
+   id: require('~/Assets/Images/indesign.svg').default,
+   figma: require('~/Assets/Images/figma.svg').default,
+};
+
+export default images;
--- src/Components/MainComponents/Content/Components/Experience/Experience.tsx
+++ src/Components/MainComponents/Content/Components/Experience/Experience.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 
 import styles from './Experience.module.scss';
 import { Grid, Row, Col } from 'GridSystem - typescript';
+import images from 'Assets/Images';
 
 const cx = classNames.bind(styles);
 
@@ -11,35 +12,144 @@ const Experience: React.FC = () => {
          <Grid>
             <Row>
                <Col size={['l-12', 'm-12', 's-12']}>
-                  <h2 className={cx('title')}>My Skill</h2>
+                  <h3 className={cx('title')}>Skills Front-End</h3>
                </Col>
             </Row>
             <Row>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.html} alt="html" />
+                     </div>
+                     <h3 className={cx('skill-name')}>HTML</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-90')}>
+                           <span>90%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.css} alt="css" />
+                     </div>
+                     <h3 className={cx('skill-name')}>CSS</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-85')}>
+                           <span>85%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.js} alt="js" />
+                     </div>
+                     <h3 className={cx('skill-name')}>JS.TS</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-80')}>
+                           <span>80%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.react} alt="react" />
+                     </div>
+                     <h3 className={cx('skill-name')}>REACT</h3>
+                     <span className={cx('skill-desc')}>Basic</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-70')}>
+                           <span>70%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
             </Row>
             <Row>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-12', 'm-12', 's-12']} className={cx('flex')}>
+                  <h3 className={cx('title')}>Skills Graphic Design</h3>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+            </Row>
+            <Row>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.ps} alt="ps" />
+                     </div>
+                     <h3 className={cx('skill-name')}>Ps</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-80')}>
+                           <span>80%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.ai} alt="ai" />
+                     </div>
+                     <h3 className={cx('skill-name')}>Ai</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-90')}>
+                           <span>90%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
-               <Col size={['l-3', 'm-6', 's-12']}>
-                  <div className={cx('box')}></div>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.id} alt="id" />
+                     </div>
+                     <h3 className={cx('skill-name')}>Id</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-85')}>
+                           <span>85%</span>
+                        </div>
+                     </div>
+                  </div>
+               </Col>
+               <Col size={['l-3', 'm-6', 's-12']} className={cx('flex')}>
+                  <div className={cx('skill')}>
+                     <div className={cx('skill-img')}>
+                        <img className={cx('img')} src={images.figma} alt="figma" />
+                     </div>
+                     <h3 className={cx('skill-name')}>Figma</h3>
+                     <span className={cx('skill-desc')}>Avanced</span>
+                  </div>
+                  <div className={cx('skill-percent')}>
+                     <div className={cx('percent')}>
+                        <div className={cx('percent-85')}>
+                           <span>85%</span>
+                        </div>
+                     </div>
+                  </div>
                </Col>
             </Row>
             <Row>
@@ -59,7 +169,7 @@ const Experience: React.FC = () => {
                <Col size={['l-8', 'm-8', 's-6']}>
                   <div className={cx('box')}></div>
                </Col>
-               <Col size={['l-4', 'm-4', 's-6']}>
+               <Col size={['l-4', 'm-4', 's-4', 'm-4', 's-6']}>
                   <div className={cx('box')}></div>
                </Col>
             </Row>
