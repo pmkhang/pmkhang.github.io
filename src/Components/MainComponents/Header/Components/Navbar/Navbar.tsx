41887482d88ab8447f5f85898371ec6c5298bfc2
Pham Minh Khang <pham.m.khang.98@gmail.com>
2023-07-23 23:48:42 +0700

    .

--- src/Components/MainComponents/Header/Components/Navbar/Navbar.tsx
+++ src/Components/MainComponents/Header/Components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 import styles from './Navbar.module.scss';
